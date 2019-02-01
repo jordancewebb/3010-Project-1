f5587b1e1e771c22a146a674587ff389e5db68e0
Jordance Webb <jordancewebb@yahoo.com>
2019-02-01 16:48:35 -0500

    Update Server.cpp

--- Update/UpdateServer/UpdateServer/Server.cpp
+++ Update/UpdateServer/UpdateServer/Server.cpp
@@ -15,7 +15,15 @@ const int  UPDATE = 2;
 int main()
 {
 	// Add your code here for the server
-	SOCKET listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	int verNum = 0;
+	SOCKET listener; 
+	listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	openInputFile(datafile,FILENAME);
+	verNum = readInt(dataFile);
+	int code = bind(listener, IPADDR, sizeOf(IPPADDR));
+	int connection = listen(listener, 1);
+	
+	
 
 	return 0;
 }
